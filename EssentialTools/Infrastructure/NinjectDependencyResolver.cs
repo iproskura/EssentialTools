b796ae55c7fbddc9fd44df21d5f92a041a61b1cf
Ihor Proskura <is.proskura@gmail.com>
2017-07-10 13:46:13 +0300

    chap. 6: added Dependency Resolver

---
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            _kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+                                   
+        private void AddBindings()
+        {
+            _kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    };
+}
---
+++ EssentialTools/Models/ShopingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShopingCart
+    {
+        private IValueCalculator _calc;
+
+        public ShopingCart(IValueCalculator calcParam)
+        {
+            _calc = calcParam;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return _calc.ValueProducts(Products);
+        }
+    }
+}
